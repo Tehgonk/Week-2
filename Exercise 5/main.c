2a4a22e0eab530066b41f7f1bf84886d04de3520
D Geula <gonkhead112@gmail.com>
2022-05-11 17:03:16 +0100

    Update main.c

---
+++ Exercise 5/main.c
@@ -0,0 +1,37 @@
+/* Exercise 5 - Odd/Even, Positive/Negative.
+ * Author: Daniel Geula
+ * Date: 10/05/2022
+ * Version: 1.0
+*/
+
+#include <stdio.h>
+
+int main() {
+
+    float userNumber;
+
+    // Prompt user for a value between -10 and 10, save as userNumber.
+    printf("\nEnter a value between -10 and 10: ");
+    scanf("%f", &userNumber);
+
+    // Check if userNumber is within the range of -10 and 10.
+    if (userNumber < -10) {
+        printf("\nInvalid value\n");
+    }
+    if (userNumber > 10) {
+        printf("\nInvalid value\n");
+    }
+
+    // Is positive/negative?
+    if (userNumber < 0) {
+        printf("\nValue is negative\n");
+    } else {
+        printf("\nValue is positive\n");
+    }
+
+    // Is odd/even?
+    if ((userNumber % 2) == 0) {
+        printf("\nValue is even\n");
+    } else {
+        printf("\nValue is odd\n")
+    }
