ab167c0e4be5a9a7233501305cf4990f9b3aea0c
D Geula <gonkhead112@gmail.com>
2022-05-11 21:18:45 +0100

    Minor tweaks and fixes

--- Exercise 6/main.c
+++ Exercise 6/main.c
@@ -6,33 +6,33 @@ int main() {
     double n1, n2;
     char op;
 
-    printf("Allowed operators: '+' '-' '/' '%' '^'\n");
+    printf("\nAllowed operators: '+' '-' '/' '%%' '^'\n");
     printf("Enter two numbers separated by an operator: ");
     scanf("%lf%c%lf", &n1, &op, &n2);
 
     switch (op) {
         case '+':
-        printf("\n%g%c%g = %g", n1, op, n2, n1 + n2);
+        printf("\n%g%c%g = %g\n", n1, op, n2, n1 + n2);
         break;
 
         case '-':
-        printf("\n%g%c%g = %g", n1, op, n2, n1 - n2);
+        printf("\n%g%c%g = %g\n", n1, op, n2, n1 - n2);
         break;
 
         case '/':
-        printf("\n%g%c%g = %g", n1, op, n2, n1 / n2);
+        printf("\n%g%c%g = %g\n", n1, op, n2, n1 / n2);
         break;
 
         case '%':
-        printf("\n%g%c%g = %g", n1, op, n2, fmod(n1, n2));
+        printf("\n%g%c%g = %g\n", n1, op, n2, fmod(n1, n2));
         break;
 
         case '^':
-        printf("\n%g%c%g = %g", n1, op, n2, pow(n1, n2));
+        printf("\n%g%c%g = %g\n\n", n1, op, n2, pow(n1, n2));
         break;
 
         default:
-        printf("\nInvalid operator");
+        printf("\nInvalid operator\n");
         break;
     }
 
