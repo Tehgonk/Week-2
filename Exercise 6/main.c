4152d9360b5f40135d3188c3d1b23236b42b976d
D Geula <gonkhead112@gmail.com>
2022-05-11 21:33:16 +0100

    Add comments

--- Exercise 6/main.c
+++ Exercise 6/main.c
@@ -6,10 +6,15 @@ int main() {
     double n1, n2;
     char op;
 
+    // Promp user to enter two numbers separated by an operator, save as n1, n2 and op.
     printf("\nAllowed operators: '+' '-' '/' '%%' '^'\n");
     printf("Enter two numbers separated by an operator: ");
     scanf("%lf%c%lf", &n1, &op, &n2);
+    
 
+    // Calculate the result of the operation on n1 and n2 using switch statement.
+    // Output the result of the operation on n1 and n2 for each case.
+    // Output an error message for the default case to handle unexpected input.
     switch (op) {
         case '+':
         printf("\n%g%c%g = %g\n", n1, op, n2, n1 + n2);
