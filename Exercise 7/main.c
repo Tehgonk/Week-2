fc3f43459de70bd5cb587c42953537a95a432163
D Geula <gonkhead112@gmail.com>
2022-05-12 17:54:25 +0100

    Create main.c

---
+++ Exercise 7/main.c
@@ -0,0 +1,15 @@
+/* Exercise 7 - Armstrong number.
+ * Author: Daniel Geula
+ * Date: 12/05/2022
+ * Version: 1.0
+*/
+
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    
+
+    return 0;
+}
