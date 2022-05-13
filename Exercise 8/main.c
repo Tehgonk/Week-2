0b9a71c5e2ae2190a1448b04e97c7eecb616b08d
D Geula <gonkhead112@gmail.com>
2022-05-13 15:48:13 +0100

    style: minor output readability changes

--- Exercise 8/main.c
+++ Exercise 8/main.c
@@ -6,16 +6,16 @@ int main() {
   char userChar;
 
   // Prompt user to enter a charachter, save as userChar.
-  printf("Enter a character: ");
+  printf("\nEnter a character: ");
   scanf("%c", &userChar);
 
   // Test userChar is uppercase or not if the ASCII value of userChar is 97 ... 122.
   if (userChar >= 'a' && userChar <= 'z') {
       // Output converted userChar using toupper() if lowercase.
-      printf("input: %c\toutput: %c", userChar, toupper(userChar));
+      printf("\nInput: %c\tOutput: %c\n", userChar, toupper(userChar));
   } else {
       // Output that userChar is already uppercase if not.
-      printf("The character you entered is already uppercase.");
+      printf("\nThe character you entered is already uppercase.\n");
   }
 
   return 0;
