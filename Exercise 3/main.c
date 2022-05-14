2604d4ae868e985e1f04bf9d2ecc55bb3652242f
D Geula <gonkhead112@gmail.com>
2022-05-14 16:05:58 +0100

    docs: update comments

--- Exercise 3/main.c
+++ Exercise 3/main.c
@@ -12,7 +12,7 @@ int main() {
 
     int randomNumber, userGuess;
 
-    // Seed the random number generator, save as randomNumber.
+    // Seed random number generator, save as randomNumber.
     srand(time(NULL));
     randomNumber = rand() % 100 + 1;
 
@@ -20,7 +20,7 @@ int main() {
     printf("\nGuess a number between 1 and 100: ");
     scanf("%d", &userGuess);
 
-    begin: // Set a label for the begin: statement.
+    begin: // Create goto label begin.
     if(userGuess == randomNumber) {
         printf("\nCongratulations! You guessed the correct number!\n");
     }
@@ -29,10 +29,9 @@ int main() {
         printf("\nToo low\n");
         printf("Try again: ");
         scanf("%d", &userGuess);
-        // Go back to the begin: statement.
-        goto begin;
+        goto begin; // Go back to the begin label.
     }
-    // Do the same for the other case.
+    // Do the same for other case.
     else {
         printf("\nToo high\n");
         printf("Try again: ");
