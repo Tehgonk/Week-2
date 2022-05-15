991f6dfc2ac946fd56bac598ddaecddc609cf543
D Geula <gonkhead112@gmail.com>
2022-05-15 18:16:58 +0100

    - refactor
    - Removed unnecessary comparisons

--- Exercise 1/main.c
+++ Exercise 1/main.c
@@ -11,27 +11,27 @@ int main() {
     float a, b, c, d, smallest, largest;
 
     // Prompt user for four numbers, save as a, b, c, d.
-    printf("Enter 4 numbers: ");
+    printf("\nEnter 4 numbers: ");
     scanf("%f %f %f %f", &a, &b, &c, &d);
 
-    // Find min, starting with whether a < b, c, d. Save as smallest if true.
+    // Find min, starting with whether a < b, c, & d. Save as smallest if true.
     // Eliminate the previous variable from the comparison each time a smaller number is found.
     if(a < b && a < c && a < d) { 
         smallest = a;
-    } else if(b < a && b < c && b < d) {
+    } else if(b < c && b < d) {
         smallest = b;
-    } else if(c < a && c < b && c < d) {
+    } else if(c < d) {
         smallest = c;
     } else {
         smallest = d;
     }
 
     // Find max by mirroring the algorithm for finding min.
-    if(a > b && a > c && a > d) {
+    if(a > b && a > c && a > d) { 
         largest = a;
-    } else if(b > a && b > c && b > d) {
+    } else if(b > c && b > d) {
         largest = b;
-    } else if(c > a && c > b && c > d) {
+    } else if(c > d) {
         largest = c;
     } else {
         largest = d;
